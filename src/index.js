df8461f24cf69b74821609d751f8b9f39ed71cd6
Greg P <gperlman27@gmail.com>
2017-07-22 17:01:41 -0400

    begin restructuring boilerplate

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 import store from './store';
 
-import App from './containers/AppContainer/App';
+import App from './App';
 
 ReactDOM.render(
   <Provider store={store}>
