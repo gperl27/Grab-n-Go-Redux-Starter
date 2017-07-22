ccc77e89b12f65226ede6fe091e8bb2752eac397
Greg P <gperlman27@gmail.com>
2017-07-22 16:29:36 -0400

    extract store out to its own file

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import reduxThunk from 'redux-thunk';
 import { BrowserRouter as Router } from 'react-router-dom';
-import reducers from './reducers';
+
+import store from './store';
 
 import App from './containers/AppContainer/App';
 
-const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
-
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <Router>
       <App />
     </Router>
